172af8ff47aaf82f1483129550ac50304bf4e73d
amWRit <derasrocks@gmail.com>
2020-12-03 13:57:00 +0545

    made road scroll and repeat

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -6,9 +6,11 @@ public class RepeatBackground : MonoBehaviour
 {
 	public Vector3 startPos;
 	private float repeatWidth;
+    private GameManagerX gameManagerController;
     // Start is called before the first frame update
     void Start()
     {
+        gameManagerController = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
         startPos = transform.position;
         repeatWidth = GetComponent<BoxCollider>().size.x/2;
     }
@@ -16,6 +18,10 @@ public class RepeatBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(gameManagerController.gameOver == false){
+            transform.Translate(Vector3.forward * Time.deltaTime * -gameManagerController.speed);
+        }
+
         if (transform.position.x < startPos.x - repeatWidth){
         	transform.position = startPos;
 		}
